0581ee89408bb9876f2174bff5d9fc23c4a34856
daniel <daniel-congo2@hotmail.com>
2025-12-11 14:23:38 +0100

    ex01 finished

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 16:05:45 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/12/11 01:06:00 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/12/11 14:23:09 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,37 @@
 
 void	invalid_signature()
 {
-	std::cout << "\n\n### TESTING INVALID SIGNATURE ###\n\n" << std::endl;
+	std::cout << "\n\n### TESTING INVALID SIGNATURE GRADE TOO SLOW ###\n\n" << std::endl;
 	Bureaucrat slave1("Daniel", 120);
-	Form form1("form1", 150, 100);
+	Form form1("form1", 10, 100);
 	try {
 		
 		slave1.signForm(form1);
 	}
 	catch (const std::exception& e) {
 		std::cerr << "Bureaucrat " << slave1.getName()
-		<< " couldn’t sign "<< form1.get_name() << " because"<< e.what() << std::endl;
+		<< " couldn’t sign "<< form1.get_name() << " because "<< e.what() << std::endl;
 	}
 }
 
-void	invalid_decrementation()
+void	valid_signature()
 {
-	std::cout << "\n\n### TESTING INVALID DECREMENTATION ###\n\n" << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
+	std::cout << "\n\n### TESTING SIGNATURE GRADE CORRECT ###\n\n" << std::endl;
+	Bureaucrat slave1("Daniel", 2);
+	Form form1("form1", 10, 100);
 	try {
-		Bureaucrat slave1("Sasuke", 50);
-		slave1.decrement_grade(200);
+		
+		slave1.signForm(form1);
 	}
 	catch (const std::exception& e) {
-		std::cerr << "Exception caught: "<< e.what() << std::endl;
-	}
-}
-
-void	invalid_incrementation()
-{
-	std::cout << "\n\n### TESTING INVALID INCREMENTATION ###\n\n" << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	try {
-		Bureaucrat slave1("Naruto", 50);
-		slave1.increment_grade(200);
-	}
-	catch (const std::exception& e) {
-		std::cerr << "Exception caught: "<< e.what() << std::endl;
+		std::cerr << "Bureaucrat " << slave1.getName()
+		<< " couldn’t sign "<< form1.get_name() << " because "<< e.what() << std::endl;
 	}
 }
 
 int	main()
 {
 	invalid_signature();
-	// invalid_decrementation();
-	// invalid_incrementation();
+	valid_signature();
 	return (0);
 }
